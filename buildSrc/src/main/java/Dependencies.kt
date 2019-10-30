56ca54857323c4a28a158b6da9b5089011470c62
Administrator <c.uvsuv@163.com>
2019-10-30 17:28:51 +0800

    升级版本

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -12,7 +12,7 @@ object Versions{
 
 object LibV{
 
-    const val recyclerview ="1.1.0-beta05"
+    const val recyclerview ="1.1.0-rc01"
     const val rxlifecycle3 ="3.1.0"
     const val rxjava = "2.2.13"
     const val rxandroid = "2.1.1"
@@ -20,8 +20,8 @@ object LibV{
     const val glide = "4.10.0"
     const val okhttp3 = "4.2.1"
     const val retrofit = "2.6.2"
-    const val lifecycle = "2.2.0-beta01"
-    const val room = "2.2.0"
+    const val lifecycle = "2.2.0-rc01"
+    const val room = "2.2.1"
     const val gson = "2.8.5"
-    const val work_runtime = "2.3.0-alpha02"
+    const val work_runtime = "2.3.0-alpha03"
 }
